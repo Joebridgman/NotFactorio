fd373a050e060e62b5fc2def22f658188c22d81c
Joe Bridgman <joebridgman13@gmail.com>
2024-09-23 19:53:10 +0100

    movement and sprite changing
    trying to get movement how id like it to feel and attempting to figure out how sprites work

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private Sprite up;
+    private Sprite down;
+    private Sprite left;
+    private Sprite right;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        up = Resources.Load("up", typeof(Sprite)) as Sprite;
+        down = Resources.Load("down", typeof(Sprite)) as Sprite;
+        left = Resources.Load("left", typeof(Sprite)) as Sprite;
+        right = Resources.Load("right", typeof(Sprite)) as Sprite;
     }
 
     // Update is called once per frame
@@ -21,18 +30,22 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             GetComponent<Rigidbody2D>().AddForce(transform.up * 100);
+            GetComponent<SpriteRenderer>().sprite = up;
         }
         if (Input.GetKey(KeyCode.S))
         {
             GetComponent<Rigidbody2D>().AddForce(transform.up * -100);
+            GetComponent<SpriteRenderer>().sprite = down;
         }
         if (Input.GetKey(KeyCode.D))
         {
             GetComponent<Rigidbody2D>().AddForce(transform.right * 100);
+            GetComponent<SpriteRenderer>().sprite = right;
         }
         if(Input.GetKey(KeyCode.A))
         {
             GetComponent<Rigidbody2D>().AddForce(transform.right * -100);
+            GetComponent<SpriteRenderer>().sprite = left;
         }
     }
 }
