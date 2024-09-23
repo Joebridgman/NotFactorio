8ffdbf41c4808b06a6fd50ec3b00397cd6f05122
Joe Bridgman <joebridgman13@gmail.com>
2024-09-23 21:38:11 +0100

    Figuring out sprite image stuff
    Figured out how to do it :)

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,18 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    private Sprite up;
-    private Sprite down;
-    private Sprite left;
-    private Sprite right;
+    public Sprite up;
+    public Sprite down;
+    public Sprite left;
+    public Sprite right;
 
     // Start is called before the first frame update
     void Start()
     {
-        up = Resources.Load("up", typeof(Sprite)) as Sprite;
-        down = Resources.Load("down", typeof(Sprite)) as Sprite;
-        left = Resources.Load("left", typeof(Sprite)) as Sprite;
-        right = Resources.Load("right", typeof(Sprite)) as Sprite;
+
     }
 
     // Update is called once per frame
